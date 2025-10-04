0aade7aa17b4911d6d226254b1aa2e5f014d32bf
Fernando Jr. <120286349+Tugasdev@users.noreply.github.com>
2025-10-04 18:15:53 +0000

    Adding specific people to the game

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 function Home() {
     return (
         <>
-            <h1>Oi eu sou o jobs</h1>
-            <h1>E eu amo jogar minecraft com a isa e abandonar o meu amigaço Tugas</h1>
+            <h1>Se for o Andrei vendo a mensagem: Opa, iae Andrei! Só queria dizer que seus comentários me dão um boost a mais pra continuar com esse curso fenomenal. Muito obrigado :D</h1>
+            <h1>Se for o Luan: Caramba, um pokemon shiny viu meu site :O Holy moly</h1>
         </>
     );
 }
