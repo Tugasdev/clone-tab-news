dd244b3e257fc4811ea622c9346e02bef87e671b
Fernando Jr. <120286349+Tugasdev@users.noreply.github.com>
2025-10-26 03:55:29 +0000

    Finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "deu certo" });
+  const updatedAt = new Date().toISOString();
+
+  const getPostgresVersion = await database.query("SHOW server_version;");
+  const postgresVersion = getPostgresVersion.rows[0].server_version;
+
+  const getMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = getMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const getUsedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const usedConnections = getUsedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        postgres_version: postgresVersion,
+        max_connections: parseInt(maxConnections),
+        used_connections: usedConnections,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const pgVersion = responseBody.dependencies.database.postgres_version;
+  expect(pgVersion).toEqual("16.0");
+
+  const maxConnections = parseInt(
+    responseBody.dependencies.database.max_connections,
+  );
+  expect(maxConnections).toEqual(100);
+
+  const usedConnections = responseBody.dependencies.database.used_connections;
+  expect(usedConnections).toEqual(1);
 });
