d279a807c7be0e669972067781a950e8f3b53d82
Fernando Jr. <120286349+Tugasdev@users.noreply.github.com>
2025-10-27 23:20:14 +0000

    Makes   more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -8,12 +8,20 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+  console.log("Credenciais do Postgres: ", {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    port: process.env.POSTGRES_PORT,
+    password: process.env.POSTGRES_PASSWORD,
+  });
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
