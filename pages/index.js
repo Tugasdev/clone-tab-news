cbf6bf5f36dea0b95cd4a3883d92d6e1bbda764b
Fernando Jr. <120286349+Tugasdev@users.noreply.github.com>
2025-12-03 06:31:34 +0000

    Adds `/api/v1/migrations` endpoint

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ function Home() {
   return (
     <>
       <h1>Olá meu querido internauta!!! Bem vindo ao meu site.</h1>
-      <p1>Ele ainda está em construção, então não tem nada por aqui :v</p1>
+      <p1>
+        Pode ser que ele um dia se torne um blog gamer pessoal... to pensando
+        nessa ideia.
+      </p1>
     </>
   );
 }
