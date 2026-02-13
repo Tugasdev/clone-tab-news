317113aa099f18a9a3dc00f9e25e52ca59952325
Fernando Jr. <120286349+Tugasdev@users.noreply.github.com>
2026-02-13 06:46:11 +0000

    Adds `lint:eslint:check` script with `ESLint` and corrected unreachable functions

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -39,13 +39,6 @@ export default async function migrations(request, response) {
       }
       return response.status(200).json(migratedMigrations);
     }
-
-    if (migrations.length > 0) {
-      return response.status(201).json(migrations);
-    }
-    return response.status(200).json(migrations);
-
-    return response.status(405).end();
   } catch (error) {
     console.error(error);
     throw error;
